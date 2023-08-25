b9d7dbe6a7446d00a540520b828341eef3fcca0b
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-08-25 15:04:31 -0400

    prettier changes

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -3,17 +3,17 @@ const { Comment } = require("../models");
 const commentData = [
   {
     comment_text: "That's great!!",
-    user_id: 1,
+    user_id: 2,
     post_id: 1,
   },
   {
     comment_text: "Any other suggestions?",
-    user_id: 2,
+    user_id: 3,
     post_id: 2,
   },
   {
     comment_text: "Meetings are every other Thursday. 😉",
-    user_id: 3,
+    user_id: 1,
     post_id: 3,
   },
 ];
